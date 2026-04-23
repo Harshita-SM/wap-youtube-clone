6c567f41c5ed91e10ce2505abcf9c4b600b4e090
Harshita <Harshita12280@gmail.com>
2026-04-23 13:25:18 +0530

    feat(header): add helpful tooltip text to navigation logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,6 +23,7 @@ function Header({ toggleSidebar }) {
                 <img 
                     src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" 
                     alt="YouTube Logo" 
+                    title="YouTube Home"
                     className='header-logo'
                 />
             </div>
